6df21fdb48d14aaac8b93fbd35ef076b37031c51
ParrvLuthra22 <parrvluthra@gmail.com>
2025-04-25 16:04:04 +0530

    Fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-// import { useState } from 'react'
-import Home from './Home.jsx'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from './components/common/Navbar';
+// import Footer from './components/common/Footer';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Services from './pages/Services';
+// import Blog from './pages/Blog';
 
 function App() {
-
   return (
-    <>
-    <Home/>
-    </>
-  )
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
