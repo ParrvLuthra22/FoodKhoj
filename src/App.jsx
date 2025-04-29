3fc57639c63add50dc44f0f923df6515088b377f
ParrvLuthra <parrvluthra@gmail.com>
2025-04-29 18:51:47 +0530

    Ninth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/common/Navbar';
-// import Footer from './components/common/Footer';
-import Home from './pages/Home';
-// import About from './pages/About';
-// import Services from './pages/Services';
-// import Blog from './pages/Blog';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Blog from "./pages/Blog";
+import NotFound from "./pages/NotFound";
 
-function App() {
-  return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/blog" element={<Blog />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      {/* Static Component Rendering */}
+      <Index />
+      <About />
+      <Services />
+      <Blog />
+      <NotFound />
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
